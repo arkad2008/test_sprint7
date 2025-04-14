b81e5861ba253b8fa10b02be26707ad8b5b43601
arkad2008 <205781768+arkad2008@users.noreply.github.com>
2025-04-14 08:41:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,45 @@ if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
 
 st.write('Esta aplicación aún no es funcional. En construcción.')
+
+
+import scipy.stats
+import streamlit as st
+import time
+
+# Título de la aplicación
+st.header('Lanzar una moneda')
+
+# Inicializar el gráfico de líneas
+chart = st.line_chart([0.5])
+
+# Función para emular el lanzamiento de monedas
+def toss_coin(n):
+    # Generar resultados de lanzamientos usando distribución Bernoulli
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    # Calcular la media y actualizar el gráfico
+    for r in trial_outcomes:
+        outcome_no += 1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])  # Añadir la media al gráfico
+        time.sleep(0.05)
+
+    return mean
+
+# Widgets de entrada para configurar el experimento
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
+
+# Acción cuando se hace clic en el botón
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    final_mean = toss_coin(number_of_trials)  # Llamar a la función de lanzamiento
+    st.write(f'La media final después de {number_of_trials} intentos es: {final_mean}')
+    
